571396775bec40e70e24e925c32b1c822d9dd5a1
TommyYow <34993026+TommyYow@users.noreply.github.com>
2018-01-09 22:16:01 +0800

    Drawer done

--- app/src/main/java/com/example/user/quizzies/LoginActivity.java
+++ app/src/main/java/com/example/user/quizzies/LoginActivity.java
@@ -40,7 +40,7 @@ public class LoginActivity extends AppCompatActivity {
         FirebaseAuth auth = FirebaseAuth.getInstance();
         //if the user is signed in, launch homepage, else launch the sign in (AuthUI) Activity
         if (auth.getCurrentUser() != null) {
-            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+            Intent i = new Intent(getApplicationContext(), Main2Activity.class);
             startActivity(i);
             finish();
             return;
@@ -87,7 +87,7 @@ public class LoginActivity extends AppCompatActivity {
 
         // Successfully signed in
         if (resultCode == RESULT_OK) {
-            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+            Intent i = new Intent(getApplicationContext(), Main2Activity.class);
             startActivity(i);
             finish();
             return;
