1ad755741d3150e959b28eed6a209e3860bf566f
TommyYow <34993026+TommyYow@users.noreply.github.com>
2018-01-11 02:47:24 +0800

    database done

--- app/src/main/java/com/example/user/quizzies/Fragment/playFragment.java
+++ app/src/main/java/com/example/user/quizzies/Fragment/playFragment.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -270,7 +271,6 @@ public class playFragment extends Fragment {
                         scaleBitmapDown(
                                 MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), uri),
                                 1200);
-
                 callCloudVision(bitmap);
                 imageViewBM.setImageBitmap(bitmap);
 
